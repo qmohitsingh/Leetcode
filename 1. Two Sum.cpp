ecca87c22e2b462fdc9bae1be529ce1a3552d315
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-02-26 18:33:55 +0530

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    
+    public int[] twoSum(int[] nums, int target) {
+        
+        Integer low=0, 
+        high=nums.length-1;
+        
+        int[] result = new int[2];
+        
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        
+        for (int i=0; i<nums.length; i++) {
+            
+            if (hashMap.containsKey(target-nums[i])) {
+                result[0] = i;
+                result[1] = hashMap.get(target-nums[i]);
+                break;
+            }
+            
+            hashMap.put(nums[i], i);
+        }
+    
+        return result;
+        
+    }
+}
+
+/*
+
+Solution: 1 using two loop
+Solution: 3 using hashmap(n + n)
+
+[2,7,11,15]
+9
+[5, 2, -2, ]
+
+[3,2,4]
+6
+[3, 4, 2]
+
+
+**/
