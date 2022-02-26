747c3c3f90c8e7aad373d41de7a227f63e99ff00
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-02-26 17:58:39 +0530

    Create 205. Isomorphic Strings.cpp

---
+++ 205. Isomorphic Strings.cpp
@@ -0,0 +1,28 @@
+// Reference : https://www.youtube.com/watch?v=6Qkail843d8
+
+class Solution {
+public:
+    
+    // Time O(n), Space O(n)
+    bool isIsomorphic(string s, string t) {
+        
+        unordered_map<char, char> map;
+        
+        unordered_map<char, int> map2;
+        for (int i=0; i<s.length(); i++) {
+            
+            if(map.find(s[i]) == map.end() && !map2[t[i]]) {
+                map[s[i]] = t[i];
+                map2[t[i]] = 1;
+            }
+            
+            if (map[s[i]] != t[i]) return false;
+            
+        }
+        
+        return true;
+        
+    }
+}
+
+
