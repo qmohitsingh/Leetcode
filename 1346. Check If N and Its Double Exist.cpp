082c64cc0060c22b2c9372ed0ea8a89dba78b39d
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-03-03 14:24:56 +0530

    Update 1346. Check If N and Its Double Exist.cpp

--- 1346. Check If N and Its Double Exist.cpp
+++ 1346. Check If N and Its Double Exist.cpp
@@ -1,15 +1,17 @@
 //https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
+// Approach #1 : Using Sorting and Binary Search 
 class Solution {
 public:
+	// Time - O(nlong), Space - O(1) 
 	bool checkIfExist(vector<int>& arr) {
 
-		sort(arr.begin(), arr.end());
+		sort(arr.begin(), arr.end()); // Time - O(nlogn)
 
-		for (int i=0; i<arr.size(); i++) {
-			if (binarySearch(arr, 2*arr[i], i)) 
+		for (int i=0; i<arr.size(); i++) {  // Time - O(n)
+			if (binarySearch(arr, 2*arr[i], i))  // Time - O(n)
 				return true;
-		}
+		} // loop and binarySearch Inside it = Time : O(nlogn)
 
 		return false;
 
@@ -30,5 +32,29 @@ public:
 		}
 
 		return nums[low] == target && low !=srcIndex;
+	}
+};
+
+// Approach #2 : Using HashMap
+class Solution {
+public:
+
+	// Time - O(n), Space - O(n)
+	bool checkIfExist(vector<int>& arr) {
+
+		unordered_map<int, int> map;
+
+		for (auto n : arr) {
+			map[n]++;
+		}
+
+		for (auto n  : arr) 
+			if (n == 0 && map[n] == 1) // Corner case
+				continue;
+			else if (map.find(n*2) != map.end()) 
+				return true;
+
+		return false;
+
 	}
 };
