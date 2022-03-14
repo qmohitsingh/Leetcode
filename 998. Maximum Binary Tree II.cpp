4f113db75fdec34e8d8b03bea5c7933ab2a84d44
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-03-14 10:42:07 +0530

    Update 998. Maximum Binary Tree II.cpp

--- 998. Maximum Binary Tree II.cpp
+++ 998. Maximum Binary Tree II.cpp
@@ -63,3 +63,38 @@ public:
         return maxIndex;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        
+        if (root == NULL) {
+            TreeNode* temp = new TreeNode(val);
+            return temp;
+        }
+        
+        if (root->val < val) {
+            TreeNode* temp = new TreeNode(val);
+            temp->left = root;
+            return temp;
+        }
+        
+        root->right = insertIntoMaxTree(root->right, val);
+        
+        return root;
+        
+    }
+};
