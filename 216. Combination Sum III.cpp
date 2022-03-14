c549d66cbc04d7a6212ff6452b4f4d7112b021d0
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-03-14 11:57:27 +0530

    Create 216. Combination Sum III.cpp

---
+++ 216. Combination Sum III.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/combination-sum-iii/
+
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    vector<int> vec;
+    int size;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        size = k;
+        dfs(0,1,n);
+        return ans;
+    }
+    
+    
+    void dfs(int sum, int index, int n) {
+        
+        if (sum > n || vec.size() > size) return;
+        
+        if (sum == n && vec.size() == size) {
+            ans.push_back(vec);
+            return;
+        }
+        
+        for (int i=index; i<=9; i++) {
+            vec.push_back(i);
+            dfs(sum+i, i+1, n);
+            vec.pop_back();
+        }
+    }
+    
+};
