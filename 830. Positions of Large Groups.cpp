f301a9c378086303047a0c9f51a00728f931c80d
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-03-20 01:04:35 +0530

    Update 830. Positions of Large Groups.cpp

--- 830. Positions of Large Groups.cpp
+++ 830. Positions of Large Groups.cpp
@@ -29,3 +29,42 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        
+        int start=0, end=0;
+        
+        vector<vector<int>> ans;
+        
+        int j = 0;
+        for (int i=0; i<s.length(); i=j) {
+            while(j < s.length() && s[j] == s[i]) j++;
+            
+            if (j - i >= 3)
+                ans.push_back({i, j-1});
+        } 
+        
+//         while (end < s.length()) {
+            
+//             if(s[end] != s[start]) {
+                
+//                 if (end - start >= 3 )
+//                     ans.push_back({start, end-1});
+                
+//                 start = end;
+//             }
+            
+//             end++;
+//         }
+        
+//         //last case
+//         if (end - start >= 3 )
+//                     ans.push_back({start, end-1});
+        
+        return ans;
+        
+    }
+};
