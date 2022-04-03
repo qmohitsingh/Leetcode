75469b1a8b3f4bac2f31868966d63dc283e4f5c6
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-04-03 16:06:31 +0530

    Update 60. Permutation Sequence.cpp

--- 60. Permutation Sequence.cpp
+++ 60. Permutation Sequence.cpp
@@ -48,3 +48,37 @@ public:
 
 // 1 2 3 4 5
 
+// Reference : https://leetcode.com/problems/permutation-sequence/discuss/22507/%22Explain-like-I'm-five%22-Java-Solution-in-O(n)
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        
+        string ans;
+        
+        vector<int> factorial(n+1);
+        
+        factorial[0] = 1;
+        for (int i=1; i<=n; i++) {
+            factorial[i] = i*factorial[i-1];
+        }
+        
+        vector<int> numbers(n+1);
+        for (int i=0; i<=n; i++) numbers[i] = i+1;
+        
+        k--; // 0 -indexing
+        auto itr = numbers.begin();
+        for (int i=1; i<=n; i++) {
+            
+            int index = k/factorial[n-i];
+            ans += to_string(numbers[index]);
+            
+            // remove that element from the list
+            numbers.erase(itr+index);
+            
+            k = k - index*factorial[n-i];
+        }
+        
+        return ans;
+        
+    }
+};
