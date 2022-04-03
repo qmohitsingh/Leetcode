747bd47d07aaed627837367daabcbb2407f426b2
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-04-03 18:58:41 +0530

    Update 1249. Minimum Remove to Make Valid Parentheses.cpp

--- 1249. Minimum Remove to Make Valid Parentheses.cpp
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -33,3 +33,47 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+        string ans;
+        
+        //From left to right iteration
+        int count = 0;
+        for(int i=0; i<s.length(); i++) {
+            
+            if(s[i] == '(') {
+                count++;
+            } else if (s[i] == ')' && count > 0) {
+                count--;
+            } else if (s[i] == ')') {
+                s[i] = '*';
+            } 
+        
+        }
+        
+        //From left to right iteration
+        count = 0;
+        for(int i=s.length()-1; i>=0; i--) {
+            
+            if(s[i] == ')') {
+                count++;
+            } else if (s[i] == '(' && count > 0) {
+                count--;
+            } else if(s[i] == '(') {
+                s[i] = '*';
+            }
+
+        }
+        
+        for(int i=0; i<s.length(); i++)
+            if (s[i] != '*') 
+                ans += s[i];
+        
+        return ans;
+    }
+};
