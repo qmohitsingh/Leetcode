372d12bcf0258001722e35b61be1a4dce5394c8c
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-04-19 23:34:02 +0530

    Create 99. Recover Binary Search Tree.cpp

---
+++ 99. Recover Binary Search Tree.cpp
@@ -0,0 +1,53 @@
+//Reference : https://walkccc.me/LeetCode/problems/0099/
+//https://leetcode.com/problems/recover-binary-search-tree/
+
+class Solution {
+ public:
+  void recoverTree(TreeNode* root) {
+    TreeNode* pred = nullptr;
+    TreeNode* x = nullptr;  // 1st wrong node
+    TreeNode* y = nullptr;  // 2nd wrong node
+
+    while (root) {
+      if (root->left) {
+        TreeNode* morrisPred = findPredecessor(root);
+        if (morrisPred->right) {  // already connected before
+          visit(root, pred, x, y);
+          morrisPred->right = nullptr;  // break the connection
+          root = root->right;
+        } else {
+          morrisPred->right = root;  // connect it!
+          root = root->left;
+        }
+      } else {
+        visit(root, pred, x, y);
+        root = root->right;
+      }
+    }
+
+    swap(x, y);
+  }
+
+ private:
+  TreeNode* findPredecessor(TreeNode* root) {
+    TreeNode* pred = root->left;
+    while (pred->right && pred->right != root)
+      pred = pred->right;
+    return pred;
+  }
+
+  void visit(TreeNode*& root, TreeNode*& pred, TreeNode*& x, TreeNode*& y) {
+    if (pred && root->val < pred->val) {
+      y = root;
+      if (!x)
+        x = pred;
+    }
+    pred = root;
+  }
+
+  void swap(TreeNode* x, TreeNode* y) {
+    const int temp = x->val;
+    x->val = y->val;
+    y->val = temp;
+  }
+};
