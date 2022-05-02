c40fcdf0eafaed5fc8e553db15764e1103b8afe3
Mohit Singh <30595867+qmohitsingh@users.noreply.github.com>
2022-05-02 22:18:06 +0530

    Create 905. Sort Array By Parity.cpp

---
+++ 905. Sort Array By Parity.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/sort-array-by-parity/
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        
+        int index = 0;
+        for (int i=0; i<A.size(); i++) {
+            if (A[i]%2 == 0) {
+                int temp = A[i];
+                A[i] = A[index];
+                A[index++] = temp;
+            }
+        }
+        
+        return A;
+    }
+};
